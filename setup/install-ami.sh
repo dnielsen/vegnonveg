303f2e38766bb0867117e4b8750b2c073e7942ce
Dave Nielsen <dnielsen@gmail.com>
2017-11-22 09:54:51 -0800

    updated by Parnay

---
+++ setup/install-ami.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update
+
+curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+bash Anaconda3-4.2.0-Linux-x86_64.sh
+
+export PATH=/root/anaconda3/bin:$PATH
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mykey.key -out mycert.pem
+conda create -y -n py27 python=2.7 anaconda
+conda install -y -c reeder java-jdk
+source activate py27
+
+conda install -y -c reeder java-jdk
+
+wget https://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz
+tar xzf spark-2.2.0-bin-hadoop2.7.tgz
+
+wget https://repo1.maven.org/maven2/com/intel/analytics/bigdl/dist-spark-2.2.0-scala-2.11.8-linux64/0.3.0/dist-spark-2.2.0-scala-2.11.8-linux64-0.3.0-dist.zip
+sudo apt-get -y install unzip
+
+mkdir dist-spark-2.2.0-scala-2.11.8-linux64-0.3.0-dist
+mv *.zip dist-spark-2.2.0-scala-2.11.8-linux64-0.3.0-dist
+cd dist-spark-2.2.0-scala-2.11.8-linux64-0.3.0-dist
+unzip *.zip
+
+pip install python-mnist
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.3.0-cp27-none-linux_x86_64.whl
+
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mykey.key -out mycert.pem
+sed -i "\$i $(echo '/./start-notebook-ami.sh')" /etc/rc.local
---
+++ setup/start-notebook-ami.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# setup paths, please use absolute path
+INSTALL_HOME=/
+export PATH=/root/anaconda3/bin:$PATH
+export JAVA_HOME="/root/anaconda3/"
+export SPARK_HOME=$INSTALL_HOME/spark-2.2.0-bin-hadoop2.7
+export BigDL_HOME=$INSTALL_HOME/dist-spark-2.2.0-scala-2.11.8-linux64-0.3.0-dist
+
+export PYSPARK_DRIVER_PYTHON=jupyter
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --allow-root --notebook-dir=./ --ip=* --no-browser --certfile=mycert.pem --keyfile=mykey.key --NotebookApp.token='bigdl'"
+VERSION=0.3.0
+source activate py27
+# tensorboard --logdir=/tmp/bigdl_summaries/lenet5-  2>&1 >tensorboard.log &
+
+if [ -z "$CORES" ]; then
+	CORES=2
+fi
+
+${SPARK_HOME}/bin/pyspark \
+  --master local[$CORES] \
+  --driver-memory 5g \
+  --properties-file ${BigDL_HOME}/conf/spark-bigdl.conf \
+  --py-files ${BigDL_HOME}/lib/bigdl-${VERSION}-python-api.zip \
+  --jars ${BigDL_HOME}/lib/bigdl-SPARK_2.2-${VERSION}-jar-with-dependencies.jar \
+  --conf spark.driver.extraClassPath=${BigDL_HOME}/lib/bigdl-SPARK_2.2-${VERSION}-jar-with-dependencies.jar \
+  --conf spark.executor.extraClassPath=${BigDL_HOME}/lib/bigdl-SPARK_2.2-${VERSION}-jar-with-dependencies.jar
+  
